99f39b148b89d3eb263bf7f63b88ef3f85d50d3c
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-04-30 18:45:33 -0500

    add ssl reject

--- server/db.js
+++ server/db.js
@@ -1,7 +1,13 @@
 const { Pool } = require("pg");
-const dataConfig = require("./configData");
 
-const pool = new Pool({ dataConfig })
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  database: process.env.DB,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+  ssl: {rejectUnauthorized: false}
+ })
 
 pool
   .connect()
