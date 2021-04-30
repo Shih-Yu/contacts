523a62a6fb6ce147046d87e0178513d9b7936f12
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-04-30 18:46:24 -0500

    delete config file and fix issue connecting to Postgres

--- server/index.js
+++ server/index.js
@@ -1,20 +1,36 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const app = express();
+const pool = require("./db");
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(logger("dev"));
+app.use(express.json());
+
+// Connection
+
 // Route
-app.get("/contacts", (req, res) => {
-  res.send({message: "hello"})
-})
-app.post("/contacts", (req, res) => {
-  console.log(req.body);
-  res.end();
-})
+// app.post("/contacts", async(req, res) => {
+//   try {
+//     const { fname, lname, email, phone, company } = req.body;
+//     const newContact = await pool.query("INSERT INTO contacts(fname, lname, email, phone, company) VALUES($1,$2,$3,$4,$5);",
+//       [fname, lname, email, phone, company]
+//     );
+//     console.log(req.body);
+  
+//   } catch (error) {
+//     console.error(err.message);
+//   }
+  
+// });
+
+app.get("/", (req, res) => {
+  res.send({ message: "hello" });
+});
 
 // listener
-app.listen(port, () => console.log(`Running on port: ${port}`))
+app.listen(port, () => console.log(`Running on port: ${port}`));
